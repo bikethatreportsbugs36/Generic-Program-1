4b18fa6109dc250bef4d6390bbcea4a6199e480a
bikethatreportsbugs36 <caniconnectyoutoabike@gmail.com>
2025-02-23 11:21:21 -0800

    stuff

--- New folder/aaaaaaaa.lua
+++ New folder/aaaaaaaa.lua
@@ -1,5 +1,3 @@
-
-
 local a = 1
 local b = 2 + 3
 local c = 4 + 5 + 6
@@ -8,8 +6,13 @@ local e = 11 + 12 + 13 + 14 + 15
 if a == 1 then
     print("a is 1")
     print(a, b, c, d, e)
-print("you get the idea")
-  end
+    print("you get the idea")
+end
+
+os.execute("start notepad.exe")  -- This will open Notepad on Windows
 
 warn("Warning at line 11: 'you get the idea' is not reachable")   
-print("this is not printeddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+-- Commented out the long print statement as it is not necessary
+-- print("this is not printeddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+local f = 16 + 17 + 18 + 19 + 20 + 21
+local g = 22 + 23 + 24 + 25 + 26 + 27 + 28 + 29 + 30 + 31 + 32 + 33 + 34 + 35 + 36 + 37 + 38  + 56 + 57 + 58 + 59 + 60 + 61 + 62 + 63 + 64 + 65 + 66 + 67 + 68 + 69 + 70 + 71 + 72 + 73 + 74 + 75 + 76 + 77 + 78 + 79 + 80 + 81 + 82 + 83 + 84 + 85 + 86 + 87 + 88 + 89 + 90 + 91 + 92 + 93 + 94 + 95 + 96 + 97 + 98 + 99 + 100
